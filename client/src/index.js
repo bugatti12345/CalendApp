9b9ea39be9248a16d151deb0f05d0d184cfa7042
Josh Liu <70559690+bugatti12345@users.noreply.github.com>
2021-04-04 08:20:44 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -10,3 +10,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
